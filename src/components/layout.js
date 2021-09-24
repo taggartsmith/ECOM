225f02ed9bbce75b4b781776ab99f4eec118bee6
Taggart Smith <taggartsmith@taggarts-mbp.lan>
2021-09-24 14:05:44 -0600

    layout header/nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
+import Header from './headernav/header';
+import Navbar from './headernav/navbar';
+
 class Layout extends Component {
   render() {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    );
+      return(
+          <div className='layout'>
+          {this.props.children}
+          <Header/>
+          <Navbar/>
+          </div>
+      )
   }
 }
 
