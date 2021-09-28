7e2c235ae375732cac68d9abfd22d18147f7d068
Taggart Smith <taggart.smith@gmail.com>
2021-09-28 13:16:45 -0600

    styling page title

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import SignInForm from "./signinForm";
 
+import PageTitle from '../pageTitle';
+
+
 class SignIn extends Component {
     onSubmit = (fields) => {
         console.log(fields);
@@ -9,8 +12,9 @@ class SignIn extends Component {
     render() {
         return (
             <div className='sign-in'>
-                <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
-            </div>
+            <PageTitle className='sign-in__page-title' title='Login' />
+            <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
+        </div>
         )
     }
 }
