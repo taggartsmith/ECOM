299153380d29d33cf328184f1f2a4c051c859a09
Taggart Smith <taggart.smith@gmail.com>
2021-09-29 10:51:25 -0600

    Made Back Button

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import SignUpForm from "./signupForm";
+
+import PageTitle from '../pageTitle';
+
 
 class SignUp extends Component {
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div className='sign-up'>
-                sign uppp
-            </div>
-        );
+            <div className='sign-in'>
+            <PageTitle className='sign-in__page-title' title='Register' />
+            <SignUpForm onSubmit={this.onSubmit} className='sign-in__form' />
+        </div>
+        )
     }
 }
 
