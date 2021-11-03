2bde852425d26f0c15d54ce52201fb63c958e5bf
Taggart Smith <taggartsmith@taggarts-mbp.lan>
2021-11-03 11:52:32 -0600

    Header bar for links

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -5,8 +5,22 @@ import {
 
 
 const INITIAL_STATE = {
-    headerLinks: [],
-    navbarLinks: []
+    headerLinks: [
+        {
+            title: 'account'
+        },
+        {
+            title: 'purchases'
+        }
+    ],
+    navbarLinks: [
+        {
+            title: 'account'
+        },
+        {
+            title: 'purchases'
+        }
+    ]
 }
 
 export default function(state = INITIAL_STATE, action) {
